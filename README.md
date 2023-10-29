7a110b0446c65292bccbc0fcdf566c5efa208a1c
darenc <dchandisingh@gmail.com>
2023-10-29 17:34:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# C2CTC
+
+C2CTC rebuilt with Go
+
+## Tech stack
+
+### Infrastructure / deployment
+
+- [Planetscale](https://app.planetscale.com/) (`mysql`)
+- Vercel (serverless functions)
+
+### Development
+
+- [HTMX](https://htmx.org)
+- [Tailwind](https://tailwindcss.com/)
+- [Alpine.js](https://alpinejs.dev/)
+
+### Go modules (external)
+
+- [GORM](https://gorm.io/)
+- [Gorilla Mux](https://pkg.go.dev/github.com/gorilla/mux)
+
+## Based on
+
+For my own reference, this project is based on the following resources:
+
+- [Connect a Go application using GORM to PlanetScale](https://planetscale.com/docs/tutorials/connect-go-gorm-app)
---
+++ api/users.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	//	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type CtcUser struct {
+	ID           int
+	UserName     string
+	UserCategory string
+	TeamID       int
+	UserAgegroup string
+}
+
+func Users(w http.ResponseWriter, r *http.Request) {
+	// Load environment variables from file.
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
+	}
+
+	// Connect to PlanetScale database using DSN environment variable.
+	db, err := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		log.Fatalf("failed to connect to PlanetScale: %v", err)
+	}
+
+	// Create an API handler which serves data from PlanetScale.
+	handler := NewHandler(db)
+
+	// Start an HTTP API server.
+	const addr = ":8080"
+	log.Printf("successfully connected to PlanetScale, starting HTTP server on %q", addr)
+	if err := http.ListenAndServe(addr, handler); err != nil {
+		log.Fatalf("failed to serve HTTP: %v", err)
+	}
+}
+
+// getCtcUsers is the HTTP handler for GET /users.
+func getCtcUsers(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	var users []CtcUser
+	result := db.Find(&users)
+	if result.Error != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "    ")
+	enc.Encode(users)
+	body, err := json.Marshal(enc)
+
+	if err != nil {
+		fmt.Printf("Error happened in JSON marshal. Err: %s", err)
+	} else {
+		w.Write(body)
+	}
+}
